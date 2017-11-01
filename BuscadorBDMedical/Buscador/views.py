a253fc918cbbb59e054bfbb43b0a653dd5f8ba31
AntonioAlcM <totidos@hotmail.com>
2017-11-01 16:43:38 +0100

    añadido en .travis.yml el comando correcto para hacer los test y añadida las cabeceras del archivo test

--- BuscadorBDMedical/Buscador/views.py
+++ BuscadorBDMedical/Buscador/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template import Context
+from django.http import JsonResponse
+from django.core import serializers
+import json
+import urllib.request
+from bson import json_util
+from functools import lru_cache
 
-# Create your views here.
+
+
+def buscar():
+    url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=cancer&reldate=60&datetype=edat&retmax=10000&usehistory=y&retmode=json'
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+    print (data)
+
+def index(request):
+    buscar()
+    return render(request, 'index.html')
---
+++ BuscadorBDMedical/BuscadorBDMedical/test.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+from django.test.client import Client
+import unittest
