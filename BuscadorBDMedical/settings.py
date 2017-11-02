97ce4a13d26c0308fc3f3979cc683b2d5278093e
AntonioAlcM <totidos@hotmail.com>
2017-11-02 17:44:58 +0100

    Quitando base de datos heroku

--- BuscadorBDMedical/settings.py
+++ BuscadorBDMedical/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,7 +127,4 @@ STATICFILES_DIRS = (
 )
 # Parse database configuration from $DATABASE_URL
 
-DATABASES['default'] =  dj_database_url.config(conn_max_age=500)
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
