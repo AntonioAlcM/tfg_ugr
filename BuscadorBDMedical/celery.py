c0793b74f03b4b2c1fd082fd76e97b1c9d4eabce
AntonioAlcM <totidos@hotmail.com>
2017-12-15 01:21:52 +0100

    cambiando ip de redis

--- BuscadorBDMedical/celery.py
+++ BuscadorBDMedical/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 from datetime import timedelta
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BuscadorBDMedical.settings')
-app = Celery('BuscadorBDMedical''BuscadorBDMedical', backend='redis://127.0.0.1:6379/0', broker='amqp://guest:guest@localhost//')
+app = Celery('BuscadorBDMedical''BuscadorBDMedical', backend='redis://0.0.0.0:6379/0', broker='amqp://guest:guest@localhost//')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
