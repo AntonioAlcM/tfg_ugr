f07d0227896f43ea47bf3dd18b72ae8c4f067c3f
AntonioAlcM <totidos@hotmail.com>
2017-12-15 01:02:30 +0100

    añadiendo requeriments y añadiendo la ruta rest

--- Buscador/urls.py
+++ Buscador/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   url(r'^rest/$', views.probando_REST, name='probandoRest'),
    url(r'^busqueda/$', busca.buscar, name='buscarDatos'),
     url(r'^recibirDatos/$', busca.enviarDatos, name='recibirDatos'),
+    url(r'^rest/$', views.probando_REST, name='probandoRest'),
   ]
